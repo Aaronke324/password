10a59db7247fe8a29f6dab3ddaac87c4d234f50e
Aaron ke <b.aaron.ke@gmail.com>
2021-05-24 21:46:23 +0800

    Updated password code

--- password.py
+++ password.py
@@ -1,12 +1,14 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	answer = input('請輸入你的密碼:')
 	if answer == password:
 		print('登入成功。')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤!還有',i ,'次機會')
-
-
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有',i ,'次機會')
+		else:
+			print('你已經輸入三次錯誤密碼，已鎖定該帳號。')
